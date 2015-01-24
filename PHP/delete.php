5a0041fafb6bbe4e84cebf5015389d2a06ef5347
karlov <kirill.karlov1@gmail.com>
2015-01-24 07:29:50 +0100

    Optimised with globe() function, delete all unused code

--- PHP/delete.php
+++ PHP/delete.php
@@ -12,19 +12,21 @@ $connection = mysql_connect(/*"simscene.mysql.ukraine.com.ua"*/"localhost", $use
 
 $db_selected = mysql_select_db($database) or die("Не могу соединиться с MySQLI.");
 
-mysql_query('SET NAMES utf8');
-
-$query = "SELECT images FROM marker WHERE code='$code'";
-$result = mysql_query($query) or die('Invalid query: ' . mysql_error());
-while ($row = @mysql_fetch_assoc($result)) {
-	$filetype = explode(',', $row['images']);
-}
-//echo $filetype[count($filetype) - 1];
-
 $query = "DELETE FROM marker WHERE code='" . $code . "'";
 
 $result = mysql_query($query) or die('Invalid query: ' . mysql_error());
 mysql_close($connection);
+
+/*$query = "SELECT images FROM marker WHERE code='$code'";
+$result = mysql_query($query) or die('Invalid query: ' . mysql_error());
+while ($row = @mysql_fetch_assoc($result)) {
+$filetype = explode(',', $row['images']);
+}*/
+
+foreach (glob("../img/" . $code . "*") as $filename) {
+	unlink($filename);
+	//$files[$i] = $filename;
+}
 
 /*$query = "DELETE FROM marker_window WHERE code='" . $code . "'";
 
@@ -34,13 +36,13 @@ $query = "DELETE FROM markerinfo WHERE code='" . $code . "'";
 
 $result = mysql_query($query) or die('Invalid query: ' . mysql_error());*/
 
-header("Content-type: text/html");
+//header("Content-type: text/html");
 //if ($filetype[0] != null) {
-for ($i = 0; $i < count($filetype) - 1; $i++) {
-	if (file_exists("../img/" . $code . $i . "." . $filetype[$i + 1])) {
-		unlink("../img/" . $code . $i . "." . $filetype[$i + 1]);
-	}
+/*for ($i = 0; $i < count($filetype) - 1; $i++) {
+if (file_exists("../img/" . $code . $i . "." . $filetype[$i + 1])) {
+unlink("../img/" . $code . $i . "." . $filetype[$i + 1]);
 }
+}*/
 //}
 
 ?>
