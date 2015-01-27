6b113339bcc67d1c2f604ca1aaf7908938447b5f
karlov <kirill.karlov1@gmail.com>
2015-01-27 22:47:21 +0100

    bugs fixed

---
+++ Admin_js/index_admin.js
@@ -0,0 +1,53 @@
+//filter free/non-free/all markers//
+	function check(type){
+		switch (type){
+			case -1:
+			markers_load(-1);
+			document.getElementById('refresh').innerHTML = "Refresh";
+			document.getElementById("free").checked = false;
+			document.getElementById("pay-free").checked = false;
+			document.getElementById("non-free").checked = false;		
+			break;
+			case 0:
+			markers_load(0);
+			document.getElementById('refresh').innerHTML = "Reset";
+			break;
+			case 1:
+			markers_load(1);
+			document.getElementById('refresh').innerHTML = "Reset";
+			break;
+			case 2:
+			markers_load(2);
+			document.getElementById('refresh').innerHTML = "Reset";
+			break;
+		}
+	}
+	setTimeout(function() {
+	document.getElementById('frame').onload = function(){
+		if(document.getElementById('frame').contentWindow.document.body.innerHTML!="" && admin){
+		alert("marker with id: " + document.getElementById("frame").contentWindow.document.body.innerHTML +" Added!"); //можливо це не потрібно
+	    markers_load("-1", document.getElementById("frame").contentWindow.document.body.innerHTML);
+	    panelinformation(document.getElementById('code').value);
+		}
+	};
+	}, 4000);
+	
+$(document).ready(function(){
+    $("#search").autocomplete({ source: source });
+
+    /*$("#ShowLogin").click(function(){
+        $("#LoginForm").toggle();
+    });
+
+    $("#LoginForm").submit(function(event){
+        Login($("#InputLogin").val(), $("#InputPassword").val());
+        event.preventDefault();
+    });*/
+	$("#scenery_free").blur(function(){
+      createfield("scenery_free");
+    });
+    $("#scenery").blur(function(){
+      createfield("scenery");
+    });
+});
+
