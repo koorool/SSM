9b8e28391b26d64be1297bc31a00371bc09ca25f
koorool <koorool@gmail.com>
2015-01-28 17:39:16 +0200

    Radio-button optimization

--- js/index.js
+++ js/index.js
@@ -1,24 +1,3 @@
-//filter free/non-free/all markers//
-	function check(type){
-		switch (type){
-			case -1:
-			markers_load(-1);
-			document.getElementById('refresh').innerHTML = "Refresh";
-			break;
-			case 0:
-			markers_load(0);
-			document.getElementById('refresh').innerHTML = "Reset";
-			break;
-			case 1:
-			markers_load(1);
-			document.getElementById('refresh').innerHTML = "Reset";
-			break;
-			case 2:
-			markers_load(2);
-			document.getElementById('refresh').innerHTML = "Reset";
-			break;
-		}
-	}
 	setTimeout(function() {
 	document.getElementById('frame').onload = function(){
 		if(document.getElementById('frame').contentWindow.document.body.innerHTML!="" && admin){
@@ -29,27 +8,20 @@
 	};
 	}, 4000);
 	
+
 $(document).ready(function(){
     $("#search").autocomplete({ source: source });
 
     $("#CheckAll").click(function(){
-    	check(-1);
+    	markers_load(-1);
     });
     $("#CheckFree").click(function(){
-    	check(0);
+    	markers_load(0);
     });
     $("#CheckPaid").click(function(){
-    	check(1);
+    	markers_load(1);
     });
     $("#CheckPaidAndFree").click(function(){
-    	check(2);
+    	markers_load(2);
     });
-    /*$("#ShowLogin").click(function(){
-        $("#LoginForm").toggle();
-    });
-
-    $("#LoginForm").submit(function(event){
-        Login($("#InputLogin").val(), $("#InputPassword").val());
-        event.preventDefault();
-    });*/
 });
