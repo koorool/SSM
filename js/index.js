2bc30108cd38332c4c34bd1921df0c0500d2e650
koorool <koorool@gmail.com>
2015-01-28 18:47:25 +0200

    Panel

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 	setTimeout(function() {
 	document.getElementById('frame').onload = function(){
-		if(document.getElementById('frame').contentWindow.document.body.innerHTML!="" && admin){
-		alert("marker with id: " + document.getElementById("frame").contentWindow.document.body.innerHTML +" Added!"); //можливо це не потрібно
+		if(document.getElementById('frame').contentWindow.document.body.innerHTML!="" && admin)
+		{
+		alert("marker with id: " + document.getElementById("frame").contentWindow.document.body.innerHTML +"Added!"); //можливо це не потрібно
 	    markers_load("-1", document.getElementById("frame").contentWindow.document.body.innerHTML);
 	    panelinformation(document.getElementById('code').value);
 		}
